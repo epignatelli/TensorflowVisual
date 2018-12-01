e6724c2682b853a6ae73b1fcfce671156cbdae5e
Eduardo <eduardo.pignatelli@burohappold.com>
2018-12-01 19:12:17 +0000

    fixing while.cs

--- Tensorflow_Engine/AutoGenOps/While.cs
+++ Tensorflow_Engine/AutoGenOps/While.cs
@@ -6,7 +6,7 @@ namespace BH.Engine.Tensorflow
 {
 public static partial class Compute
 	{
-		public static TensorFlow.TFOutput[] While(this TFGraph graph, TensorFlow.TFOutput[] inputs, TensorFlow.TFGraph+WhileConstructor constructor, TensorFlow.TFStatus status=null)
+		public static TensorFlow.TFOutput[] While(this TFGraph graph, TensorFlow.TFOutput[] inputs, TensorFlow.TFGraph constructor, TensorFlow.TFStatus status=null)
 		{
 			return graph.While(inputs, constructor, status);
 		}
