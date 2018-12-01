19988d4c68dead0f352e5aeed2b43f5273351a82
Eduardo <eduardo.pignatelli@burohappold.com>
2018-12-01 23:57:01 +0000

    Added run session method

---
+++ Tensorflow_Engine/Compute/Run.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TensorFlow;
+
+namespace BH.Engine.Tensorflow
+{
+    public static partial class Compute
+    {
+        public static TFTensor[] Run(this TFGraph graph, TFOutput[] inputNodes, TFTensor[] inputValue, TFOutput[] outputNode)
+        {
+            using (TFSession sess = new TFSession(graph))
+            {
+                return sess.Run(inputNodes, inputValue, outputNode);
+            }
+        }
+
+        public static TFTensor[] Run(this TFGraph graph, TFOutput inputNodes, TFTensor inputValue, TFOutput outputNode)
+        {
+            using (TFSession sess = new TFSession(graph))
+            {
+                return sess.Run(new TFOutput[] { inputNodes }, new TFTensor[] { inputValue }, new TFOutput[] { outputNode });
+            }
+        }
+    }
+}
