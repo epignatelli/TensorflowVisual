8eb79f8ff333e39bd75e4c193b8bf8b09772f05c
Eduardo <eduardo.pignatelli@burohappold.com>
2018-12-01 23:56:38 +0000

    Tensor evaluation added

---
+++ Tensorflow_Engine/Query/Value.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TensorFlow;
+
+namespace BH.Engine.Tensorflow
+{
+    public static partial class Query
+    {
+        public static object Value(TFTensor tensor)
+        {
+            return tensor.GetValue();
+        }
+
+        public static List<object> Value(TFTensor[] tensor)
+        {
+            return tensor.Select(t => t.GetValue()).ToList();
+        }
+    }
+}
